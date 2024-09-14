51730d18d537dbce7418323dc2225b7b634b951e
Emmanuel <kipronofb@outlook.com>
2024-09-14 14:31:26 +0300

    update: image name

--- local/build.sh
+++ local/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -e;
-docker buildx build \
-    --platform linux/amd64 \
-    --build-arg BUILDKIT_INLINE_CACHE=1 \
-    -t europe-west1-docker.pkg.dev/owa-gemini/docker-registry/spark-master:latest . --push;
+# docker buildx build \
+#     --platform linux/amd64 \
+#     --build-arg BUILDKIT_INLINE_CACHE=1 \
+#     -t europe-west1-docker.pkg.dev/owa-gemini/docker-registry/spark-master:latest . --push;
 
 
 
 poetry run ansible-playbook playbooks/build.yml \
-    --extra-vars "europe-west1-docker.pkg.dev/owa-gemini/docker-registry" \
+    --extra-vars "registry=europe-west1-docker.pkg.dev/owa-gemini/docker-registry" \
     --extra-vars "project_dir=$(pwd)" \
