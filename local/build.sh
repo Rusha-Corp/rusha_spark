d382efcb7a176ec695afb32e2c475b8c40dadd5c
Emmanuel <kipronofb@outlook.com>
2024-10-11 17:32:47 +0300

    update: build script

--- local/build.sh
+++ local/build.sh
@@ -2,7 +2,7 @@
 
 set -e;
 docker buildx build \
-    --platform linux/amd64,linux/arm64 \
+    --platform linux/amd64 \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
     -t europe-west1-docker.pkg.dev/owa-gemini/docker-registry/spark-master:latest . --push;
 
