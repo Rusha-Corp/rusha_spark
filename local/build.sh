7802f1326142ccaf01cb56502869324e1078ea97
Emmanuel <kipronofb@outlook.com>
2024-12-16 15:46:02 +0300

    update: build env

--- local/build.sh
+++ local/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 set -e;
-docker buildx build \
-    --platform linux/amd64 \
-    --build-arg BUILDKIT_INLINE_CACHE=1 \
-    -t europe-west1-docker.pkg.dev/owa-gemini/docker-registry/rusha-spark-environment:3.5.0-scala2.12-java17-python3.10.12-ubuntu-latest . --push;
-
-
 
 poetry run ansible-playbook playbooks/build.yml \
     --extra-vars "registry=europe-west1-docker.pkg.dev/owa-gemini/docker-registry" \
