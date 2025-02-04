6e085bcfa25db469863301eaed35710c5d626b13
Emmanuel <emmanuel@amband.co.ke>
2025-02-04 13:29:07 +0100

    update: include netcat for health checks

--- local/up_node.sh
+++ local/up_node.sh
@@ -7,4 +7,4 @@ export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 export AWS_SESSION_TOKEN=$(aws configure get aws_session_token)
 
-docker compose --profile spark-node up --remove-orphans -d  #--build
+docker compose --profile spark-node up --remove-orphans -d  --build
